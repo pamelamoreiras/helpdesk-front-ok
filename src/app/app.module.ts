74f3cffba4c40c205bb22cb79052b9cd40493702
Pamela Moreira <pamelamoreiras99@gmail.com>
2022-08-18 17:31:27 -0300

    HTML e CSS para componente NAV

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -24,11 +24,15 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatCardModule } from '@angular/material/card';
 
+// componentes do projeto
+import { NavComponent } from './components/nav/nav.component';
+
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
