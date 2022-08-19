39ef46a4aef363b202a64ef8b6f3256401bce28f
Pamela Moreira <pamelamoreiras99@gmail.com>
2022-08-19 13:58:13 -0300

    HTML e CSS do componente Tecnico list

---
+++ src/app/components/tecnico/tecnico-list/tecnico-list.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { Tecnico } from 'src/app/models/tecnico';
+
+@Component({
+  selector: 'app-tecnico-list',
+  templateUrl: './tecnico-list.component.html',
+  styleUrls: ['./tecnico-list.component.css']
+})
+export class TecnicoListComponent implements OnInit {
+
+  ELEMENT_DATA: Tecnico[] = [
+    {
+      id: 1,
+      nome: 'Pamela Moreira',
+      cpf: '14496107651',
+      email: 'pamela@gmail.com',
+      senha: '123',
+      perfis: ['0'],
+      dataCriacao: '19/08/2022'
+    }
+  ]
+
+  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol', 'acoes'];
+  dataSource = new MatTableDataSource<Tecnico>(this.ELEMENT_DATA);
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
+
+}
