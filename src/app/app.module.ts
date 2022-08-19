a57ed9c99a2f9bab206d91eb6b12a6c355c60ad5
Pamela Moreira <pamelamoreiras99@gmail.com>
2022-08-19 17:14:23 -0300

    Serviço de mensagens Implementado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -30,6 +30,8 @@ import { HomeComponent } from './components/home/home.component';
 import { HeaderComponent } from './components/header/header.component';
 import { TecnicoListComponent } from './components/tecnico/tecnico-list/tecnico-list.component';
 import { LoginComponent } from './components/login/login.component';
+import { ToastrModule } from 'ngx-toastr';
+import { timeout } from 'rxjs';
 
 
 
@@ -62,7 +64,12 @@ import { LoginComponent } from './components/login/login.component';
     MatInputModule,
     MatIconModule,
     MatListModule,
-    MatCardModule
+    MatCardModule,
+    ToastrModule.forRoot({
+      timeOut: 4000,
+      closeButton: true,
+      progressBar: true
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
