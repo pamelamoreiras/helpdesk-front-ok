70649e1dcc84211d1508ec6a4008a9b616a668de
Pamela Moreira <pamelamoreiras99@gmail.com>
2022-08-25 15:57:45 -0300

    Rota, HTML e CSS de chamado update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -43,6 +43,7 @@ import { ClienteCreateComponent } from './components/cliente/cliente-create/clie
 import { ClienteUpdateComponent } from './components/cliente/cliente-update/cliente-update.component';
 import { ChamadoListComponent } from './components/chamado/chamado-list/chamado-list.component';
 import { ChamadoCreateComponent } from './components/chamado/chamado-create/chamado-create.component';
+import { ChamadoUpdateComponent } from './components/chamado/chamado-update/chamado-update.component';
 
 @NgModule({
   declarations: [
@@ -61,6 +62,7 @@ import { ChamadoCreateComponent } from './components/chamado/chamado-create/cham
     ClienteListComponent,
     ChamadoListComponent,
     ChamadoCreateComponent,
+    ChamadoUpdateComponent,
   ],
   imports: [
     BrowserModule,
