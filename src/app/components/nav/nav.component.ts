b3449b1af3fba0997f80b45a93bf8b83b2e7b9bb
Pamela Moreira <pamelamoreiras99@gmail.com>
2022-08-26 09:20:03 -0300

    componente chamado read finalizado

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -16,7 +16,7 @@ export class NavComponent implements OnInit {
     private toast: ToastrService) { }
 
   ngOnInit(): void {
-    this.router.navigate(['chamados/update/:id'])
+    this.router.navigate(['chamados/read/:id'])
   }
 
   logout(){
